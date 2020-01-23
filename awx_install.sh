093cf907b34b15e4e413bf989eb66cf9ea08fad3
alandry5925 <59928246+alandry5925@users.noreply.github.com>
2020-01-23 15:20:45 -0600

    Update awx_install.sh

--- awx_install.sh
+++ awx_install.sh
@@ -6,16 +6,17 @@ systemctl enable firewalld;
 systemctl start firewalld;
 firewall-cmd --add-service=http --permanent;firewall-cmd --add-service=https --permanent;
 systemctl restart firewalld;
-echo "Putting SElinux in Permissive Mode";
+tput setaf 3;echo "Putting SElinux in Permissive Mode"; tput setaf 7;
 sleep 1;
 setenforce 0;
+#Below omitted line causes the system to disable SElinux enforcement completely. Uncomment at will.
 #sed -i 's|SELINUX=enforcing|SELINUX=disabled|g' /etc/selinux/config;
 yum -y install epel-release;
 yum install -y git gcc gcc-c++ lvm2 bzip2 gettext nodejs yum-utils device-mapper-persistent-data python-pip python36 ansible.noarch;
 yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo;
 yum -y install docker
 systemctl start docker && systemctl enable docker;
-echo "Attempting PIP install of docker Python, if setup fails past this point proceed manually";
+tput setaf 1; echo "Attempting PIP install of docker Python, if setup fails past this point PROCEED MANUALLY"; tput setaf 7;
 pip3 install docker-compose;
 sleep 1;
 echo "Installing PIP version of Selinux"
@@ -26,5 +27,5 @@ cd awx/installer/;
 echo "Changing Interpreter to PY3";
 sed -i 's|/usr/bin/env python|/usr/bin/python3|g' /root/awx/installer/inventory;
 ansible-playbook -i inventory install.yml;
-echo "install complete, access AWX using HOST IP om port 80";
+tput setaf 2; echo "Install complete, access your AWX instance using a web browser pointed at this HOST"; tput setaf 7;
 sleep 2;
