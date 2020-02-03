664e1a5c29c0261c1081f3a1e046ada384f7017e
Austin L <59928246+alandry5925@users.noreply.github.com>
2020-02-03 14:41:33 -0600

    Update boto_deploy_aws.sh

--- boto_deploy_aws.sh
+++ boto_deploy_aws.sh
@@ -6,4 +6,8 @@ touch credentials;
 echo [default] >> credentials && echo aws_access_key_id = YOUR-ACCESS-KEY-HERE >> credentials && echo aws_secret_access_key = YOUR-SECRET-ACCESS-KEY-HERE;
 touch config;
 echo [default] >> config && echo region = us-west-1 >> config;
-
+cd ~ && wget https://raw.githubusercontent.com/alandry5925/ScriptLab/master/boto_test.py;
+echo Configure your IAM AWS Access keys in ~/.aws/credentials to enable connectivity.;
+echo Run boto_test.py to get a returned sample output of able-to-reach s3 bucket names;
+sleep 2;
+echo End of script;
