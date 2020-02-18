0a518746d86162275fca16f56ba3d81618e269a0
Austin L <59928246+alandry5925@users.noreply.github.com>
2020-02-17 19:05:38 -0600

    Update gimu-chancli_installer.sh

--- gimu-chancli_installer.sh
+++ gimu-chancli_installer.sh
@@ -3,17 +3,17 @@
 #Date: 02/17/2020
 #Title: 4chan CLI browser installer
 tput setaf 1; echo Script made for CENTOS 7 Distros!;
-echo "chancli Author:" 
-tput setaf 6; echo "Gimu (https://github.com/Gimu/chancli)"; tput setaf 7;
+tput setaf 6; echo "chancli Author:" 
+echo "Gimu (https://github.com/Gimu/chancli)"; tput setaf 7;
 sleep 2;
 tput setaf 2; echo Installing chancli using GIT; tput setaf 7;
 echo Press "[ENTER] to Continue";
 tput setaf 4;read prompt;tput setaf 7;
 yum install -y git python36;
 pip3 install urwid;
-cd /home/$USER/;
+cd ~;
 git clone https://github.com/Gimu/chancli.git;
-echo "alias 4chan='python3 /home/$USER/chancli/chancli.py'" >> ~/.bashrc;
+echo "alias 4chan='python3 ~/chancli/chancli.py'" >> ~/.bashrc;
 sleep 1;
 tput setaf 3; echo "chancli script installed, Alias has been set to '4chan'"; tput setaf 7;
 echo "Dependencies Installed: Python3.6 and git";
