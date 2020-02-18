07438c72646302a24d596f2c6b45fdfa1257e86d
Austin L <59928246+alandry5925@users.noreply.github.com>
2020-02-17 19:19:14 -0600

    Update gimu-chancli_installer.sh

--- gimu-chancli_installer.sh
+++ gimu-chancli_installer.sh
@@ -4,9 +4,11 @@
 #Title: 4chan CLI browser installer
 tput setaf 1; echo Script made for CENTOS 7 Distros!;
 tput setaf 6; echo "chancli Author:" 
-echo "Gimu (https://github.com/Gimu/chancli)"; tput setaf 7;
+echo "Gimu (https://github.com/Gimu/chancli)";tput setaf 7;
 sleep 2;
 tput setaf 2; echo Installing chancli using GIT; tput setaf 7;
+tput setaf 1; echo "You will need to re-log in order for this install to complete.";
+echo "The script will force a logout at the end, interrupt the script to cancel";tput setaf 7;
 echo Press "[ENTER] to Continue";
 tput setaf 4;read prompt;tput setaf 7;
 yum install -y git python36;
@@ -14,11 +16,13 @@ pip3 install urwid;
 cd ~;
 git clone https://github.com/Gimu/chancli.git;
 echo "alias 4chan='python3 ~/chancli/chancli.py'" >> ~/.bashrc;
-echo "Sourcing ~/.bashrc for refresh of aliases";
-source /etc/bash.bashrc;
 sleep 1;
 tput setaf 3; echo "chancli script installed, Alias has been set to '4chan'"; tput setaf 7;
 echo "Dependencies Installed: Python3.6 and git";
 echo "Installed Python Packges: urwid";
-echo "Closing script, enter '4chan' in your shell to launch."
+echo "Closing script, enter '4chan' in your shell on system log-in to launch."
 sleep 1;
+tput setaf 3;echo "RELOG NECESSARY, we need to refresh your alias in ~/.bashrc";
+echo Press "[ENTER] to Log out";
+read prompt;tput setaf 7;
+logout;
